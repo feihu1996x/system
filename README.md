211ddc10e76bc28811a49a1cb23bbb5597a61b75
feihu1996x <gsx0312@qq.com>
2018-09-17 13:37:05 +0800

    project init

---
+++ README.md
@@ -0,0 +1,12 @@
+QQ群爬虫项目
+=====================
+
+```
+
+## 启动 web server
+python3 manage.py runserver
+
+## 执行任务脚本
+
+
+```
---
+++ config/base_setting.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+SERVER_PORT = 8999
+DEBUG = True
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1/mysql?charset=utf8mb4'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENCODING = "utf8mb4"

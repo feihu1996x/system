d1ce511e29bd054588628750cc7abbf35c6a85b5
feihu1996x <gsx0312@qq.com>
2018-09-30 18:59:45 +0800

    QQ群消息爬虫完善

--- config/auto_export_group_msgs_setting.py
+++ config/auto_export_group_msgs_setting.py
@@ -11,18 +11,18 @@ INTERVAL = 0
 
 # qq账号配置
 QQ_ACCOUNT_LIST = [
-    {
-        "qq_number": "770761627",
-        "qq_password": "QQ0119870915!LHT"
-    },
+    # {
+    #     "qq_number": "770761627",  # TODO:暂时不能登录
+    #     "qq_password": "QQ0119870915!LHT"
+    # },
     {
         "qq_number": "347946089",
         "qq_password": "QQqwert!qwe"
-    },     
+    },    
     {
         "qq_number": "3414664932",
         "qq_password": "qq19870915!lht"
-    },        
+    },       
     {
         "qq_number": "474753408",
         "qq_password": "dadada123"

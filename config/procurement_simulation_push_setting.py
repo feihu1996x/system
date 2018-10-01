544da900ba6d17e29c13632e31c6933441442912
feihu1996x <gsx0312@qq.com>
2018-10-02 03:18:43 +0800

    update files

--- config/procurement_simulation_push_setting.py
+++ config/procurement_simulation_push_setting.py
@@ -1,5 +1,8 @@
 # 定时执行间隔，单位是秒，若值为0，则表示一次性执行完
 INTERVAL = 0
 
-# 接口调用地址
+# 接口调用地址(开发环境)
 PUSH_URL = 'http://m.dev.baozizhuli.com/api/requirement/issue'
+
+# 接口调用地址(生产环境)
+# PUSH_URL = 'http://m.baozizhuli.com/api/requirement/issue'

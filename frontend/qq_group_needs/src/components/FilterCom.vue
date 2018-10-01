c0bbf53fc6f8ec481c297e7b7e331a3e3ee50572
feihu1996x <gsx0312@qq.com>
2018-10-01 12:37:38 +0000

    update files

--- frontend/qq_group_needs/src/components/FilterCom.vue
+++ frontend/qq_group_needs/src/components/FilterCom.vue
@@ -4,8 +4,8 @@
     <legend>过滤</legend>
     </fieldset>  
     <button class="layui-btn layui-btn-lg layui-btn-primary layui-btn-radius"><router-link :to="{ name: 'FilterKeys' }">管理过滤关键词</router-link></button>
-    <button class="layui-btn layui-btn-lg layui-btn-primary layui-btn-radius"><router-link :to="{ name: 'FilterGroupNumbers' }">管理过滤QQ群号码</router-link></button>
-    <button class="layui-btn layui-btn-lg layui-btn-primary layui-btn-radius"><router-link :to="{ name: 'FilterQqNumbers' }">管理过滤QQ号码</router-link></button>
+    <button class="layui-btn layui-btn-lg layui-btn-primary layui-btn-radius"><router-link :to="{ name: 'FilterGroupNumbers' }">管理过滤QQ/微信群号码</router-link></button>
+    <button class="layui-btn layui-btn-lg layui-btn-primary layui-btn-radius"><router-link :to="{ name: 'FilterQqNumbers' }">管理过滤QQ/微信号码</router-link></button>
   </div>
 </template>
 

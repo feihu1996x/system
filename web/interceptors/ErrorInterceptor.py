7c7026dcf1f3a1565a03963aef8dda309fe6c42e
feihu1996x <gsx0312@qq.com>
2018-10-02 15:06:23 +0800

    增加统一错误请求拦截器（404）

---
+++ web/interceptors/ErrorInterceptor.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+@file: ErrorInterceptor.py
+@brief: 统一错误请求拦截器
+@author: feihu1996.cn
+@date: 18-10-02
+@version: 1.0
+"""
+
+import json
+
+from flask import make_response
+
+from application import app
+
+
+@app.errorhandler( 404 )
+def error_404( e ):
+    resp_data = {
+        "code": 0,
+        "count": 0,
+        "msg": "操作失败~",
+        "data": []
+    }    
+
+    app.logger.info( str( e ) )
+
+    resp_data['msg'] = str( e )
+
+    response = make_response( json.dumps( resp_data ), 404 )
+    response.headers["Content-Type"] = "application/json;charset=utf-8"
+    return response
--- www.py
+++ www.py
@@ -2,6 +2,7 @@
 from application import app
 from web.controllers.index import route_index
 from web.controllers.messages.Message import route_message
+from web.interceptors.ErrorInterceptor import *
 
 app.register_blueprint( route_index,url_prefix = app.config["URL_PREFIX"] + "/" )
 app.register_blueprint( route_message,url_prefix = app.config["URL_PREFIX"] + "/message" )
